984ffdd87c18cf1db776da12afef3abffb00c81d
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-07-25 11:31:15 +0200

    Add faker.js

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,11 @@
 import { it, expect, describe } from "vitest";
+import { faker } from "@faker-js/faker";
 
 describe("group", () => {
   it("should", () => {
-    expect(1).toBeTruthy();
+    console.log({
+      name: faker.commerce.productName(),
+      price: faker.commerce.price({ min: 1, max: 100 }),
+    });
   });
 });
