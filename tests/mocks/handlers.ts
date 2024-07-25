641862ba99291581dbbef97c33912d4c4e6050f4
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-07-25 12:17:19 +0200

    Add faker db

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,22 +1,3 @@
-import { http, HttpResponse } from "msw";
-import { products } from "./data";
+import { db } from "./db";
 
-export const handlers = [
-  http.get("/categories", () => {
-    return HttpResponse.json([
-      { id: 1, name: "Electronics" },
-      { id: 2, name: "Beauty" },
-      { id: 3, name: "Gardening" },
-    ]);
-  }),
-
-  http.get("/products", (req) => {
-    return HttpResponse.json(products);
-  }),
-  http.get("/products/:id", ({ params }) => {
-    const id = parseInt(params.id as string);
-    const product = products.find((p) => p.id === id);
-    if (!product) return new Response(null, { status: 404 });
-    return HttpResponse.json(product);
-  }),
-];
+export const handlers = [...db.product.toHandlers("rest")];
